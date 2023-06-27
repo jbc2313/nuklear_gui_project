1f2c1fc965bfac8d463c401e6b54c0152ee1a980
jb <101614021+jbc2313@users.noreply.github.com>
2023-06-26 23:49:05 -0500

    Added basic code to file_manager
    Trying to figure out nuklear

--- src/file_manager.c
+++ src/file_manager.c
@@ -2,5 +2,12 @@ static void
 file_manager(struct nk_context *ctx)
 {
 
+    if (nk_begin(ctx, "File_Manager", nk_rect(10, 10, 180, 250),
+        NK_WINDOW_BORDER|NK_WINDOW_NO_SCROLLBAR|NK_WINDOW_MOVABLE))
+    {
 
+        nk_layout_row_dynamic(ctx, 35, 1);
+        nk_button_label(ctx, "C")
+    }
+    nk_end(ctx);
 }
