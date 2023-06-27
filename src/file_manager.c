2e8296e5dbfa4fd451b9cbb10962fe9120c1ac61
jb <101614021+jbc2313@users.noreply.github.com>
2023-06-27 00:23:40 -0500

    Updated code
    added minimize to file_manager gui window

--- src/file_manager.c
+++ src/file_manager.c
@@ -3,7 +3,7 @@ file_manager(struct nk_context *ctx)
 {
 
     if (nk_begin(ctx, "File_Manager", nk_rect(10, 10, 180, 250),
-        NK_WINDOW_BORDER|NK_WINDOW_NO_SCROLLBAR|NK_WINDOW_MOVABLE))
+        NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
     {
 
         nk_layout_row_dynamic(ctx, 35, 1);
